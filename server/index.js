b9495ed4f7c1c2f374a1c248a152e337aad1625a
Sajjan Vaidya <sajjanrajvaidya@gmail.com>
2020-06-08 13:52:27 -0700

    Make an API call for artist search

--- server/index.js
+++ server/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const axios = require('axios');
+const config = require('./config');
 
 const port = 8080;
 
@@ -17,11 +19,27 @@ app
 
 router.get('/getArtist', (req, res) => {
   const { name } = req.query;
+  // const config = {
+  //   method: 'get',
+  //   url: `https://api.spotify.com/v1/search?q=${name}&type=artist`,
+  //   headers: {
+  //     'Authorization': config.auth,
+  //   },
+  // };
 
-
-
-
-
-
-  res.end();
+  // axios.get(`https://api.spotify.com/v1/search?q=${name}&type=artist`, {
+  axios.get('https://api.spotify.com/v1/search', {
+    headers: {
+      'Authorization': config.auth,
+    },
+    params: {
+      q: name,
+      type: 'artist',
+    }
+  })
+  .then(data => res.send(data.data.artists.items.slice(0, 3)))
+  .catch((err) => {
+    console.error('An error occured', err);
+    res.send(400);
+  }); // remove err from response
 })
