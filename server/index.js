3f7a40d1b77f6e6b6f6af89b11821e4aaa92ada9
Sajjan Vaidya <sajjanrajvaidya@gmail.com>
2020-06-08 13:24:18 -0700

    Add router to server

--- server/index.js
+++ server/index.js
@@ -1,13 +1,22 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+// const routes = require('./routes');
 
 const port = 8080;
+
+const router = express.Router();
 
 app
   .use(express.json())
   .use(express.urlencoded({ extended: true }))
   .use(express.static(path.join(__dirname, '../public')))
+  .use('/', router)
   .listen(port, () => {
     console.log(`Server is listening on port: ${ port }`);
   })
+
+router.get('/getArtist', (req, res) => {
+  console.log('Hit!');
+  res.end();
+})
